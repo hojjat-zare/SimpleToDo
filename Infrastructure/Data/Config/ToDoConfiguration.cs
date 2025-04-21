11710797f892a52c6ff0ad4092414f2623666835
Hojjat <hjt1377@gmail.com>
2025-04-21 20:03:12 +0330

    add TodoConfiguration

---
+++ Infrastructure/Data/Config/ToDoConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CoreBusiness.Entities.ToDo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config;
+
+internal class ToDoConfiguration : IEntityTypeConfiguration<ToDo>
+{
+    public void Configure(EntityTypeBuilder<ToDo> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+               .IsRequired()
+               .HasMaxLength(50);
+
+        builder.Property(x => x.Description)
+               .HasMaxLength(500);
+
+        builder.Property(x => x.Status)
+               .IsRequired();
+    }
+}
