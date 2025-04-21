7969ad5b09d481c9eaffd60462ce19586be2926d
Hojjat <hjt1377@gmail.com>
2025-04-21 19:52:02 +0330

    add Repository and ToDoContext

---
+++ Infrastructure/Data/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using CoreBusiness.Interfaces;
+
+
+
+
+namespace Infrastructure.Data;
+public class EfRepository<T> : RepositoryBase<T>, IReadRepository<T>, IRepository<T> where T : class, IAggregateRoot
+{
+    public EfRepository(ToDoContext dbContext) : base(dbContext)
+    {
+    }
+}
---
+++ Infrastructure/Data/ToDoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreBusiness.Entities.ToDo;
+
+namespace Infrastructure.Data;
+
+public class ToDoContext : DbContext
+{
+    public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ToDo> ToDos { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ToDoContext).Assembly);
+    }
+}
