f2365c315d96b985a0e1664e29de3a83ebdfd72e
Hojjat <hjt1377@gmail.com>
2025-04-21 20:30:29 +0330

    add database configuration to PublicApi

--- PublicApi/Program.cs
+++ PublicApi/Program.cs
@@ -1,6 +1,33 @@
+using CoreBusiness.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+bool useOnlyInMemoryDatabase = false;
+if (builder.Configuration["UseOnlyInMemoryDatabase"] != null)
+{
+    useOnlyInMemoryDatabase = bool.Parse(builder.Configuration["UseOnlyInMemoryDatabase"]!);
+}
+
+if (useOnlyInMemoryDatabase)
+{
+    builder.Services.AddDbContext<ToDoContext>(c =>
+       c.UseInMemoryDatabase("SimpleToDoDb"));
+}
+else
+{
+    builder.Services.AddDbContext<ToDoContext>(c =>
+        c.UseSqlServer(builder.Configuration.GetConnectionString("ToDoContext")));
+}
+
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+builder.Services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
+
+
+
+// Add builder.Services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
